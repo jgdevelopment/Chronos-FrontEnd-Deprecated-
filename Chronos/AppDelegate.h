1d704161852a9600a229d18a2fc028f35e7bb7d6
Jason Ginbserg <ginsdaman@gmail.com>
2014-01-06 19:11:49 -0500

    Initial Commit

---
+++ Chronos/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Chronos
+//
+//  Created by Jason Ginsberg on 1/6/14.
+//  Copyright (c) 2014 Jason Ginsberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
